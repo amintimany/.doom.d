76d3e8ae9d42c1cb1ec1feba1bea2d6d9739c3c5
Amin Timany <amintimany@gmail.com>
2021-04-02 20:52:53 +0200

    Starting to experiment with Doom Emacs

---
+++ config.el
@@ -0,0 +1,99 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Amin Timany"
+      user-mail-address "amintimany@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; Set locale
+(setenv "LANG" "en_US.UTF-8")
+;; Increase the default font size
+(set-face-attribute 'default nil :height 150)
+;; Force emacs ask yes no question when exiting
+(setq confirm-kill-emacs 'y-or-n-p)
+
+(setq initial-frame-alist '((width . 120) (height . 50)))
+
+;; magit key binding
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;;  git-gutter-mode
+(global-git-gutter-mode +1)
+
+;;  sml-modeline-mode
+(sml-modeline-mode t)
+
+;; configuring auctex
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-PDF-mode t)
+(setq TeX-source-correlate-method (quote synctex))
+(setq TeX-source-correlate-mode t)
+
+(setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+(setq TeX-view-program-list
+     '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -r %n %o %b")))
+
+;; Coq and ProofGeneral
+(add-hook 'coq-mode-hook 'column-enforce-mode)
+
+(add-hook 'coq-mode-hook 'enable-show-trailing-whitespace)
+
+(setq coq-double-hit-enable t)
+
+;; Load company-coq when opening Coq files
+(add-hook 'coq-mode-hook #'company-coq-mode)
+;; Disable symbol prettification
+(setq company-coq-disabled-features '(prettify-symbols))
