1e47940ad7c6d5d992ad92c7b29cfdb0aee6c494
Amin Timany <amintimany@gmail.com>
2021-04-05 21:02:05 +0200

    got tired of evil mode

--- config.el
+++ config.el
@@ -95,3 +95,18 @@
 (add-hook 'coq-mode-hook #'company-coq-mode)
 ;; Disable symbol prettification
 (setq company-coq-disabled-features '(prettify-symbols))
+
+
+;;------------------------------------------------------------------------------
+;; Bind C-c C-- to toggling comments.
+
+(defun comment-or-uncomment-line-or-region ()
+  "Comments or uncomments the current line or region."
+  (interactive)
+  (if (region-active-p)
+      (comment-or-uncomment-region (region-beginning) (region-end))
+    (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    )
+  )
+
+(global-set-key (kbd "C-c C--") 'comment-or-uncomment-line-or-region)
